3bb9db81eaf4801dfacf739e45e0a1be5df2c5f4
casey <caseymwangi31@gmail.com>
2024-04-16 11:09:34 +0300

    adding changes

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeElements = document.querySelectorAll(".like-glyph");
+
+//iterating over each heart
+likeElements.forEach(likeElement =>{
+//starting by setting the hearts to empty
+likeElement.textContent = EMPTY_HEART;
+//invoking the server call
+mimicServerCall().then(() => {
+
+}).catch((error) => {
+    const errorMessage = document.getElementById("modal-message");
+    errorMessage.textContent = error;
+});
+
+
+likeElement.onclick = (event) => {
+  if (event.target.textContent === EMPTY_HEART){
+    event.target.textContent = FULL_HEART
+    event.target.classList.add('activated-heart');
+  } else {
+    event.target.textContent = EMPTY_HEART
+    event.target.classList.remove('activated-heart');
+  }
+
+//Selecting the hidden modal
+const hiddenModal = document.querySelector(".hidden");
+
+
+hiddenModal.classList.remove("hidden");
+
+
+setTimeout(() => {
+  hiddenModal.classList.add("hidden");
+}, 3000);
+};
+});
 
 
 
